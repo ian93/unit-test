b577a1a91708c45ca56fefc68ae26d8be85ba2a1
陳彥霖 Ian <ian-chen@systex.com>
2024-09-20 10:27:57 +0800

    chore: Project initialize

---
+++ src/test/java/com/example/ut/UtApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.ut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
